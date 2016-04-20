69fcfe2f0a318b3490591496e47a5ca0c0b264ad
Chad Specter <chad.specter@gmail.com>
2016-04-20 09:33:01 -0400

    changed talib version

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
     name: 'cspecter:talib',
-    version: '0.0.1',
+    version: '0.0.2',
     summary: 'Load Ta-lib NPM package.',
     git: "https://github.com/cspecter/meteor_talib",
     documentation: "README.md"
 });
 
 Npm.depends({
-    "talib": "0.6.0"
+    "talib": "^1.0.2"
 });
 
 Package.onUse( function( api ) {
